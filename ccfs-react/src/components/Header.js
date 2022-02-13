d83361c0c41f499c2ec43304fc2a4954797346ba
Maandarr <athompson@northislandcollege.ca>
2022-02-13 12:36:32 -0800

    Create main components js files

---
+++ ccfs-react/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="Header">
+        <header>
+        <h1>Cumberland Community Forest Society</h1>
+
+        </header>
+    </div>
+  )
+}
+
+export default Header
