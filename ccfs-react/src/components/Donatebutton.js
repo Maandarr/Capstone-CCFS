c67cd4fe17bfe3d753a9614194f6a3d63d1a2619
Maandarr <athompson@northislandcollege.ca>
2022-02-13 13:57:21 -0800

    Correct button name

--- ccfs-react/src/components/Donatebutton.js
+++ ccfs-react/src/components/Donatebutton.js
@@ -1,10 +1,11 @@
-import './Navbutton.css'
+import React from 'react'
+import './Donatebutton.css'
 import { Link } from 'react-router-dom'
 
-export function Navbutton() {
+export function Donatebutton() {
     return (
         <Link to='donate'>
             <button className='btn'><i class="fa-solid fa-tree" />Donate Today</button>
         </Link>
-    )
+    );
 }
