bfeca8b49edb1890f1427ac58c25658838fe302e
Maandarr <athompson@northislandcollege.ca>
2022-02-13 13:02:18 -0800

    Import BrowserRouter and add Navbar to the App

--- ccfs-react/src/App.js
+++ ccfs-react/src/App.js
@@ -1,5 +1,15 @@
+import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom';
+// import Header from './components/Header'
+// import Footer from './components/Footer'
+import Navbar from './components/Navbar';
+
 function App() {
   return (
+    <Router>
+      <Navbar />
+    </Router>
   );
 }
+
 export default App;
