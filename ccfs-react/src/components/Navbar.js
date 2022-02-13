140c432887844e59593c14340a67674bb541fa53
Maandarr <athompson@northislandcollege.ca>
2022-02-13 13:40:11 -0800

    Add nav list items and set up drop downs

--- ccfs-react/src/components/Navbar.js
+++ ccfs-react/src/components/Navbar.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react'
-import { Navbutton } from './Navbutton'
 import { Link } from 'react-router-dom'
 import './css/Navbar.css'
 import Dropdown from './Dropdown'
 
 function Navbar() {
-    const [click, setClick] = useState(false)
+    const [click, setClick] = useState(false);
+    const [dropdown, setDropdown] = useState(false);
 
     const handleClick = () => setClick(!click)
+    const closeMobileMenu = () => setClick(false)
 
     return (
         <>
@@ -17,8 +18,36 @@ function Navbar() {
                 </Link>
                 <div className='menu-icon' onClick={handleClick}>
                     <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
-
                 </div>
+                <ul className={ click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className='nav-item'>
+                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                            The Forest
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/getInvolved' className='nav-links' onClick={closeMobileMenu}>
+                            Get Involved <i className='fas fa-caret-down' />
+                        </Link>
+                        {dropdown && <Dropdown />}
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/the-ccfs' className='nav-links' onClick={closeMobileMenu}>
+                            The CCFS <i className='fas fa-caret-down' />
+                        </Link>
+                        {dropdown && <Dropdown />}
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/contact' className='nav-links' onClick={closeMobileMenu}>
+                            Contact
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/donate' className='nav-links' onClick={closeMobileMenu}>
+                            Donate Today
+                        </Link>
+                    </li>
+                </ul>
             </nav>
         </>
     );
