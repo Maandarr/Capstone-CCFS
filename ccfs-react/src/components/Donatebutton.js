459ccc3e627cdbae0e2876307bc3f9de1ec4a157
Maandarr <athompson@northislandcollege.ca>
2022-02-13 21:54:09 -0800

    Change donate button icon

--- ccfs-react/src/components/Donatebutton.js
+++ ccfs-react/src/components/Donatebutton.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 export function Donatebutton() {
     return (
         <Link to='donate'>
-            <button className='btn'><i class="fa-solid fa-tree" />Donate Today</button>
+            <button className='btn'><i class="fas fa-tree" />Donate Today</button>
         </Link>
     );
 }
