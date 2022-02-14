7ef4cbe5e1083fae97ffffed8948c0cc15ac811c
Maandarr <athompson@northislandcollege.ca>
2022-02-13 21:21:16 -0800

    Add open and close drop down function for desktop views

--- ccfs-react/src/components/Navbar.js
+++ ccfs-react/src/components/Navbar.js
@@ -5,6 +5,8 @@ import GIDropdown from './GIDropdown'
 import CCFSDropdown from './CCFSDropdown'
 
 
+
+
 function Navbar() {
     const [click, setClick] = useState(false);
     const [GIdropdown, setGIDropdown] = useState(false);
@@ -13,6 +15,20 @@ function Navbar() {
 
     const handleClick = () => setClick(!click)
     const closeMobileMenu = () => setClick(false)
+
+    const GIonMouseEnter = () => {
+        (window.innerWidth < 960) ? setGIDropdown(false) : setGIDropdown(true)
+    }
+    const GIonMouseLeave = () => {
+        (window.innerWidth < 960) ? setGIDropdown(false) : setGIDropdown(false)
+    }
+    const CCFSonMouseEnter = () => {
+        (window.innerWidth < 960) ? setCCFSDropdown(false) : setCCFSDropdown(true)
+    }
+    const CCFSonMouseLeave = () => {
+        (window.innerWidth < 960) ? setCCFSDropdown(false) : setCCFSDropdown(false)
+    }
+    
 
     return (
         <>
@@ -25,17 +41,21 @@ function Navbar() {
                 </div>
                 <ul className={ click ? 'nav-menu active' : 'nav-menu'}>
                     <li className='nav-item'>
-                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                        <Link to='/the-forest' className='nav-links' onClick={closeMobileMenu}>
                             The Forest
                         </Link>
                     </li>
-                    <li className='nav-item'>
+                    <li className='nav-item' 
+                        onMouseEnter={GIonMouseEnter}
+                        onMouseLeave={GIonMouseLeave}>
                         <Link to='/getInvolved' className='nav-links' onClick={closeMobileMenu}>
                             Get Involved <i className='fas fa-caret-down' />
                         </Link>
                         {GIdropdown && <GIDropdown />}
                     </li>
-                    <li className='nav-item'>
+                    <li className='nav-item' 
+                        onMouseEnter={CCFSonMouseEnter}
+                        onMouseLeave={CCFSonMouseLeave}>
                         <Link to='/the-ccfs' className='nav-links' onClick={closeMobileMenu}>
                             The CCFS <i className='fas fa-caret-down' />
                         </Link>
