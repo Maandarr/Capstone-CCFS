96c0830aac44f6c14b923554f63ef01f918c6222
Maandarr <athompson@northislandcollege.ca>
2022-03-11 11:49:53 -0800

    Add Footer structure and import footer navigation and subscription CTA form

--- ccfs-react-app/src/components/Footer.js
+++ ccfs-react-app/src/components/Footer.js
@@ -1,11 +1,23 @@
 import React from "react"
 import './css/Footer.css'
+import FooterNav from "./FooterNav"
+// import SubscribeForm from "./FooterForm"
+import FooterTest from "./FooterTest"
 
 const Footer = () => {
   return (
-    <div className="Footer">
-      <footer>copyright CCFS - Amanda Thompson</footer>
+    <div className="footer-container">
+      <footer>
+        <div className="social">
+          <i className="fab fa-facebook"></i>
+          <i className="fab fa-instagram"></i>
+          <i className="fab fa-youtube"></i>
+        </div>
+        <FooterNav />
+        <FooterTest />
+      </footer>
     </div>
+
   )
 }
 
