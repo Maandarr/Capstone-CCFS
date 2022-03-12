b788573977419b947471e56ba8e8abcfbbf682bb
Maandarr <athompson@northislandcollege.ca>
2022-03-11 20:37:32 -0800

    Add video header to home page

--- ccfs-react-app/src/components/pages/home.js
+++ ccfs-react-app/src/components/pages/home.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import backgroundVideo from '../videos/lets-protect-our-forest.mp4'
+// import ReactPlayer from 'react-player'
+import '../css/home.css'
 
 export default function Home() {
+    // const VIDEO_WIDTH = 1920;
+    // const VIDEO_HEIGHT = 600;
     return (
-        <div>
-            <h1 className='home'>Welcome to the Forest</h1>
+        <div className='home-container'>
+            <h1 className='home-title'>Welcome to the Forest</h1>
+            <video autoPlay loop muted id='video'>
+                <source src={backgroundVideo} type='video/mp4' />
+            </video>
         </div>
     )
 }
