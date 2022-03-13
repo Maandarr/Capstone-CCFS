925d2e0684246bf7035acdfa4e47039d19cefc63
Maandarr <athompson@northislandcollege.ca>
2022-03-13 00:05:26 -0800

    Add card section with images, headings, text, links. Two columns with padding - card styling

--- ccfs-react-app/src/components/pages/home.js
+++ ccfs-react-app/src/components/pages/home.js
@@ -37,6 +37,27 @@ export default function Home() {
                 <p>Read More &gt; &gt; &gt;</p>
             </Link>
             <SubscriptionCTABanner />
+            <section className='card-section'>
+                <div className="card-container">
+                    <div className="card">
+                        <img src={require("../../images/banner-boardwalk.jpeg")} alt="Forest board walk" />
+                        <h2>Save Our Forest</h2>
+                        <p>The Cumberland Forest is part of an interconnected system of forests, creeks, wetlands and riparian areas that form part of both the Trent and Puntledge Watersheds. The ecosystem is an important link that connects the mountains, rivers and lakes of Vancouver Island to the shores of the Salish Sea.</p>
+                        <Link to='/the-forest' className='card-read-more'>
+                            <p>Read More &gt; &gt;</p>
+                        </Link>
+                    </div>
+                    <div className="card">
+                        <img src={require("../../images/banner-babyferns-1184x592.jpeg")} alt="Ferns in the forest" />
+                        <h2>You Can Help</h2>
+                        <p>There are many ways that you can support this exciting community conservation effort. You can become a monthly donor, a one time contributor or develop a unique business campaign to raise funds. You can also share your skills, talents and energy as a volunteer for one of our super fun events.</p>
+                        <Link to='/donate' className='card-read-more'>
+                            <p>Read More &gt; &gt;</p>
+                        </Link>
+                    </div>
+                </div>
+
+            </section>
         </div>
     )
 }
