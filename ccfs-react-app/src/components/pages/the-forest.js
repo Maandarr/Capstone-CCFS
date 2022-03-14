2bb111a8866d7ae000810b19cdfa727cd5be6ad1
Maandarr <athompson@northislandcollege.ca>
2022-03-14 15:35:15 -0700

    Add heroku ssh public key

--- ccfs-react-app/src/components/pages/the-forest.js
+++ ccfs-react-app/src/components/pages/the-forest.js
@@ -3,8 +3,12 @@ import '../css/the-forest.css'
 
 export default function Forest() {
     return (
-        <>
+        <div className='forest-container'>
             <h1 className='banner-title'>What is the Cumberland Forest?</h1>
-        </>
+            <section>
+
+            </section>
+
+        </div>
     )
 }
