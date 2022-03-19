20fbfbcc19ca102dbb4c8dc150f44ed865d9c2bf
Maandarr <70283410+Maandarr@users.noreply.github.com>
2022-03-19 14:34:25 -0700

    Add live donation form

--- src/components/pages/donate.js
+++ src/components/pages/donate.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { ReactDOM } from 'react'
+import '../css/donate.css'
 
 export default function Donate() {
     return (
         <>
             <h1 className='banner-title'>Donate Today</h1>
-        </>
+            <iframe className='donation-form'
+      src="https://www.canadahelps.org/en/dn/m/57684/donation"
+    
+      ></iframe>
+
+      </>
+       
     )
 }
