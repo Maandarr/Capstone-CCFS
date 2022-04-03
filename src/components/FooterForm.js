fd755005fce2530dd06e13a1843f08d47258e715
Maandarr <athompson@northislandcollege.ca>
2022-04-02 23:39:50 -0700

    Add label for forms

--- src/components/FooterForm.js
+++ src/components/FooterForm.js
@@ -1,46 +1,49 @@
 import React from "react";
 
 class SubscribeForm extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {value: ''};
-  
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
-  
-   
-  
-    handleSubmit(event) {
-      alert('A name was submitted: ' + this.state.value);
-      event.preventDefault();
-      const data = new FormData(event.target);
-    
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+    const data = new FormData(event.target);
+
     fetch('/api/form-submit-url', {
       method: 'POST',
       body: data,
     });
-    }
-  
-    render() {
-      return (
-        <form onSubmit={this.handleSubmit}>
-            <input type="text" 
-            id="name"
-            name="name" 
-            placeholder="Your Name" 
-            aria-labelledby="name"
-            required/>
-            <input 
-            id="email"
-            type="email" 
-            name="email" 
-            placeholder="Your Email" 
-            aria-labelledby="email"
-            required/>
-          <input id="subscribe" type="submit" value="Subscribe" aria-labelledby="subscribe"/>
-        </form>
-      );
-    }
   }
 
-  export default SubscribeForm
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="name" className="visually-hidden">Name</label>
+        <input type="text"
+          id="name"
+          name="name"
+          placeholder="Your Name"
+          aria-labelledby="name"
+          required />
+        <label htmlFor="email" className="visually-hidden">Email</label>
+
+        <input
+          id="email"
+          type="email"
+          name="email"
+          placeholder="Your Email"
+          aria-labelledby="email"
+          required />
+        <input id="subscribe" type="submit" value="Subscribe" aria-labelledby="subscribe" />
+      </form>
+    );
+  }
+}
+
+export default SubscribeForm
