ca958192c0d7a5eb58e0f4afc719a2c6568355e9
Maandarr <70283410+Maandarr@users.noreply.github.com>
2022-04-02 21:59:42 -0700

    Remove unused pages/css

--- src/App.js
+++ src/App.js
@@ -4,12 +4,10 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/home'
 import Forest from './components/pages/the-forest'
-import Volunteer from './components/pages/volunteer'
 import CCFS from './components/pages/the-ccfs'
 import Donate from './components/pages/donate'
 import Contact from './components/pages/contact'
 import Board from './components/pages/board'
-import GetInvolved from './components/pages/getInvolved'
 import Footer from './components/Footer'
 
 function App() {
@@ -19,8 +17,6 @@ function App() {
       <Routes>
         <Route path='/' exact element={<Home />} />
         <Route path='/the-forest' exact element={<Forest />} />
-        <Route path='/getInvolved' exact element={<GetInvolved />} />
-        <Route path='/volunteer' exact element={<Volunteer />} />
         <Route path='/donate' exact element={<Donate />} />
         <Route path='/the-ccfs' exact element={<CCFS />} />
         <Route path='/board' exact element={<Board />} />
--- src/components/pages/volunteer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Volunteer() {
-    return (
-        <>
-            <h1 className='banner-title'>Volunteer Sign-Up</h1>
-        </>
-    )
-}
